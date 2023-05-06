78f87d163318791729ae8eac4745689c2f014a40
Jason A. Cox <jason@jasonacox.com>
2023-05-05 21:48:58 -0700

    Add clouds

--- tools/cron.sh
+++ tools/cron.sh
@@ -3,30 +3,36 @@
 # Cronjob to check Powerwall battery charge level and
 # adjust the reserve limit. 
 #
-# This script is set to optimize clean energy usage by 
-# having the Powerwall charge batteries during peak
+# This script is set to optimize Powerwall charging during
 # solar production (clean energy) and use battery during
-# peak grid usage (dirty energy) time during WINTER.
+# peak grid usage (dirty energy) time. 
 #
 # Requires:
 #  * pypowerwall python module (pip install pypowerwall)
 #  * Tesla auth file setup - see instructions at:
 #    https://github.com/jasonacox/pypowerwall/tree/main/tools
-#
+#  * weather411 service (optional)
+#    (see https://github.com/jasonacox/Powerwall-Dashboard/tree/main/weather)
 
 # SET THIS 
 POWERWALL='10.1.1.10'     # address of Powerwall
+WEATHER='10.1.1.11'       # address of weather411 service
 PASSWORD='yourPassword'   # Powerwall password
 FOLDER='/home/tesla'      # Location of Tesla auth file
 
 # Reserve Settigs
-MAX=80                    # Reserve level for charge mode
-MIN=20                    # Minimum reserve level for discharge
+MAX=80
+MIN=20
+CLOUDS=0
 
 LOGFILE=cron.log
 cd $FOLDER
 
-# Fetch current stats from Powerwall
+# Fetch cloud conditions from jasonacox/weather411 container
+# Note: comment out if you do not have weather411 service
+CLOUDS=`curl --silent http://${WEATHER}:8676/clouds | jq -r '.clouds'`
+
+# Fetch current stats from Powerwall 1=grid, 2=house, 3=solar, 4=pw, 5=level, 6=reserve
 STATE=`python3 << END
 import pypowerwall
 pw = pypowerwall.Powerwall("${POWERWALL}","${PASSWORD}")
@@ -49,21 +55,24 @@ HOUR=`date +%H`
 MINUTE=`date +%M`
 H=`date +%H | bc` # remove leading zero
 M=`date +%M | bc`
-echo "$MONTH/$DATE/$YEAR ${HOUR}:${MINUTE}: The battery level is ${LEVEL}, Grid=${GRID}, House=${HOUSE}, Solar=${SOLAR}, PW=${PW}, Reserve Setting=${CUR}"
+echo "$MONTH/$DATE/$YEAR ${HOUR}:${MINUTE}: The battery level is ${LEVEL}, Grid=${GRID}, House=${HOUSE}, Solar=${SO
+LAR}, PW=${PW}, Reserve Setting=${CUR}, Clouds=${CLOUDS}"
 
 # Function to change reserve
 change() {
     echo "Change to ${1}"
     /usr/bin/python3 set-reserve.py --set $1
-    echo "$MONTH/$DATE/$YEAR ${HOUR}:${MINUTE}: Updated to ${1} - The battery level is ${LEVEL}, Grid=${GRID}, House=${HOUSE}, Solar=${SOLAR}, PW=${PW}, Reserve was=${CUR}" >> $LOGFILE
+    echo "$MONTH/$DATE/$YEAR ${HOUR}:${MINUTE}: Updated to ${1} - The battery level is ${LEVEL}, Grid=${GRID}, Hous
+e=${HOUSE}, Solar=${SOLAR}, PW=${PW}, Reserve was=${CUR}" >> $LOGFILE
 }
 
-# Logic blocks for operations
+# Logic for operations
 
 # WINTER - Nov, Dec and Jan - Adjust Reserve to save energy for peak
 if (( "${MONTH}" == "11" )) || ((  "${MONTH}" == "12")) || (( "${MONTH}" == "01" )); then
-    # From 9am to 3pm - Peak solar production time - charge battery
-    if (( $H >= 9 )) && (( $H < 15 )); then
+    # From 9am to 4pm - Peak solar production time - charge battery
+    if (( $H >= 9 )) && (( $H < 16 )); then
+        # 9am to 4pm
         if (( $(echo "$LEVEL < $MAX" |bc -l) )); then
             # If not charged
             if (( $(echo "$CUR < $MAX" |bc -l) )); then
@@ -72,11 +81,9 @@ if (( "${MONTH}" == "11" )) || ((  "${MONTH}" == "12")) || (( "${MONTH}" == "01"
             fi
         fi
     fi
-fi
-
-# SPRING - Feb, Mar, Apr - Check for Clouds
-if (( "${MONTH}" == "02" )) || ((  "${MONTH}" == "03")) || (( "${MONTH}" == "04" )); then
-    # From 9am to 3pm - Peak solar production time - charge battery
+else
+# NOT WINTER
+    # From 11am to 4pm - Peak solar production time - charge battery if cloudy
     if (( $H >= 11 )) && (( $H < 16 )) && (( $CLOUDS > 90 )); then
         # 11am to 4pm
         if (( $(echo "$LEVEL < $MAX" |bc -l) )); then
@@ -89,8 +96,8 @@ if (( "${MONTH}" == "02" )) || ((  "${MONTH}" == "03")) || (( "${MONTH}" == "04"
     fi
 fi
 
-# Afternoon after 3pm - Peak grid usage - switch to battery
-if (( $H >= 15 )); then
+# Afternoon - Peak grid usage - force switch to battery
+if (( $H >= 16 )); then
     if (( $(echo "$CUR > $MIN" |bc -l) )); then
         # change reserve if not already set
         change $MIN
