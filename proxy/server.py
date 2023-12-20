e8c71b28278aac7047e09d1af18df286bb151c54
Jason Cox <jason@jasonacox.com>
2023-12-20 00:18:11 -0800

    Build t31

--- proxy/server.py
+++ proxy/server.py
@@ -28,7 +28,7 @@ import signal
 import ssl
 from transform import get_static, inject_js
 
-BUILD = "t30"
+BUILD = "t31"
 ALLOWLIST = [
     '/api/status', '/api/site_info/site_name', '/api/meters/site',
     '/api/meters/solar', '/api/sitemaster', '/api/powerwalls', 
