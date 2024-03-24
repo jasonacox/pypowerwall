f9b6fff64af81d3bcc3c3dfb8453dc2591412b3d
Igor Cherkaev <igor.cherkaev@copart.com>
2024-03-24 17:23:53 -0500

    Make messages consistent across log levels for @not_implemented_mock_data

--- pypowerwall/cloud/decorators.py
+++ pypowerwall/cloud/decorators.py
@@ -13,7 +13,7 @@ def not_implemented_mock_data(func):
                         "printed only once at the warning level.")
             WARNED_ONCE[func.__name__] = 1
         else:
-            log.debug("This API is using mock data in cloud mode.")
+            log.debug(f"This API [{func.__name__}] is using mock data in cloud mode.")
         return func(*args, **kwargs)
 
     return wrapper
