2e55e861ee0d6aab37073aa6c407da4f3e00cb0b
Jason Cox <jason@jasonacox.com>
2024-06-01 15:22:25 -0700

    Display summary config and status

--- tools/tedapi/tedapi.py
+++ tools/tedapi/tedapi.py
@@ -291,29 +291,53 @@ if __name__ == "__main__":
 
     # Print Header
     print("Tesla Powerwall Gateway TEDAPI Reader")
+    print()
 
     # Command line arguments
     if len(sys.argv) > 1:
         gw_pwd = sys.argv[1]
-        print(f" - Using Powerwall Gateway Password: {gw_pwd}")
     else:
         # Get GW_PWD from User
         gw_pwd = input("\nEnter Powerwall Gateway Password: ")
-    # Create TEDAPI Object
+
+    # Create TEDAPI Object and get Configuration and Status
+    print(f"Connecting to Powerwall Gateway {GW_IP}")
     ted = TEDAPI(gw_pwd)
-    # Get and save configuration to file
     config = ted.get_config()
-    with open('config.json', 'w') as f:
-        json.dump(config, f)
-    # Get and save status to file
     status = ted.get_status()
+    print()
+
+    # Print Configuration
+    print(" - Configuration:")
+    site_info = config.get('site_info', {})
+    site_name = site_info.get('site_name', 'Unknown')
+    print(f"   - Site Name: {site_name}")
+    battery_commission_date = site_info.get('battery_commission_date', 'Unknown')
+    print(f"   - Battery Commission Date: {battery_commission_date}")
+    vin = config.get('vin', 'Unknown')
+    print(f"   - VIN: {vin}")
+    number_of_powerwalls = len(config.get('battery_blocks', []))
+    print(f"   - Number of Powerwalls: {number_of_powerwalls}")
+    print()
+
+    # Print power data
+    print(" - Power Data:")
+    nominalEnergyRemainingWh = status.get('control', {}).get('systemStatus', {}).get('nominalEnergyRemainingWh', 0)
+    nominalFullPackEnergyWh = status.get('control', {}).get('systemStatus', {}).get('nominalFullPackEnergyWh', 0)
+    soe = round(nominalEnergyRemainingWh / nominalFullPackEnergyWh * 100, 2)
+    print(f"   - Battery Charge: {soe}% ({nominalEnergyRemainingWh}Wh of {nominalFullPackEnergyWh}Wh)")
+    meterAggregates = status.get('control', {}).get('meterAggregates', [])
+    for meter in meterAggregates:
+        location = meter.get('location', 'Unknown').title()
+        realPowerW = int(meter.get('realPowerW', 0))
+        print(f"   - {location}: {realPowerW}W")
+    print()
+
+    # Save Configuration and Status to JSON files
     with open('status.json', 'w') as f:
         json.dump(status, f)
+    with open('config.json', 'w') as f:
+        json.dump(config, f)
     print(" - Configuration and Status saved to config.json and status.json")
-    ask = input(" - Do you want to print the configuration and status files? (y/N): ")
-    if ask.lower() == 'y':
-        print(" - Configuration:")
-        print(json.dumps(config, indent=4))
-        print(" - Status:")
-        print(json.dumps(status, indent=4))
+    print()
 
