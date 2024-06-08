d6bde91a7a3891b0d1067f2037367c8061cdc809
Jason Cox <jason@jasonacox.com>
2024-06-08 10:44:03 -0700

    Update cron.sh to poll InfluxDB for max 24hr temp

--- tools/cron.sh
+++ tools/cron.sh
@@ -16,6 +16,7 @@
 
 # SET THIS 
 POWERWALL='10.1.1.10'     # address of Powerwall
+#INFLUXDB='10.1.1.20'     # address of InfluxDB - Comment out if not using Powerwall-Dashboard
 WEATHER='10.1.1.11'       # address of weather411 service
 PASSWORD='yourPassword'   # Powerwall password
 FOLDER='/home/tesla'      # Location of Tesla auth file
@@ -39,6 +40,22 @@ pw = pypowerwall.Powerwall("${POWERWALL}","${PASSWORD}")
 print("%d,%d,%d,%d,%d,%d" % (pw.grid(),pw.home(),pw.solar(),pw.battery(),pw.level(True),pw.get_reserve(True)))
 END`
 
+# Powerwall-Dashboard users: Python funtion to get max temperature from InfluxDB for past 24 hours
+MAXTEMP=0
+if [ ! -z "$INFLUXDB" ]; then
+MAXTEMP=`python3 << END
+import influxdb
+client = influxdb.InfluxDBClient("${INFLUXDB}", database='powerwall')
+query = 'SELECT max("temp_max") FROM "autogen"."weather" WHERE time > now() - 24h GROUP BY time(1d) fill(none)'
+result = client.query(query)
+points = list(result.get_points())
+if len(points) > 0:
+    print(points[0]['max'])
+else:
+    print(0)
+END`
+fi
+
 # Data from pypowerwall 1=grid, 2=house, 3=solar, 4=pw, 5=level
 GRID=`echo ${STATE} | cut -f1 -d,`
 HOUSE=`echo ${STATE} | cut -f2 -d,`
@@ -60,7 +77,12 @@ echo "$MONTH $DATE $YEAR ${HOUR}:${MINUTE}: The battery level is ${LEVEL}, Grid=
 # Function to change reserve
 change() {
     echo "Change to ${1}"
-    /usr/bin/python3 set-reserve.py --set $1
+    # if variable is current
+    if [ "$1" == "current" ]; then
+        /usr/bin/python3 set-reserve.py --current
+    else
+        /usr/bin/python3 set-reserve.py --set $1
+    fi
     echo "$MONTH $DATE $YEAR ${HOUR}:${MINUTE}: Updated to ${1} - The battery level is ${LEVEL}, Grid=${GRID}, House=${HOUSE}, Solar=${SOLAR}, PW=${PW}, Reserve was=${CUR}" | tee -a $LOGFILE
 }
 
@@ -95,10 +117,19 @@ else
 fi
 
 # Afternoon - Peak grid usage - force switch to battery
-if (( $H >= 16 )); then
+if (( $H >= 16 )) && (( $H < 21 )); then
+    if (( $(echo "$CUR <= $MAX" |bc -l) )); then
+        if (( $(echo "$CUR > $MIN" |bc -l) )); then
+            # change reserve if not already set
+            change $MIN
+        fi
+    fi
+fi
+
+# Evening 9pm to Midnight - Stop using battery if 24h max temp was above 25C
+if (( $H >= 21 )) && (( $MAXTEMP > 25 )); then
     if (( $(echo "$CUR > $MIN" |bc -l) )); then
-        # change reserve if not already set
-        change $MIN
+        change "current"
     fi
 fi
 
