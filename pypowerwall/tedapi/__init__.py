f836c3c7e21165230e71fd2d33bb78daa113900f
Christopher Pitstick <cmp@pitstick.net>
2025-05-14 21:37:57 -0400

    Reducing pool size and making verify false by default

--- pypowerwall/tedapi/__init__.py
+++ pypowerwall/tedapi/__init__.py
@@ -153,7 +153,7 @@ class TEDAPI:
         # Fetch DIN from Powerwall
         log.debug("Fetching DIN from Powerwall...")
         url = f'https://{self.gw_ip}/tedapi/din'
-        r = requests.get(url, auth=('Tesla_Energy_Device', self.gw_pwd), verify=False, timeout=self.timeout)
+        r = self.session.get(url, auth=('Tesla_Energy_Device', self.gw_pwd), timeout=self.timeout)
         if r.status_code in BUSY_CODES:
             # Rate limited - Switch to cooldown mode for 5 minutes
             self.pwcooldown = time.perf_counter() + 300
@@ -233,7 +233,7 @@ class TEDAPI:
             pb.tail.value = 1
             url = f'https://{self.gw_ip}/tedapi/v1'
             try:
-                r = self.session.post(url, verify=False, data=pb.SerializeToString(), timeout=self.timeout)
+                r = self.session.post(url, data=pb.SerializeToString(), timeout=self.timeout)
                 log.debug(f"Response Code: {r.status_code}")
                 if r.status_code in BUSY_CODES:
                     # Rate limited - Switch to cooldown mode for 5 minutes
@@ -334,7 +334,7 @@ class TEDAPI:
             pb.tail.value = 1
             url = f'https://{self.gw_ip}/tedapi/v1'
             try:
-                r = self.session.post(url, verify=False, data=pb.SerializeToString(), timeout=self.timeout)
+                r = self.session.post(url, data=pb.SerializeToString(), timeout=self.timeout)
                 log.debug(f"Response Code: {r.status_code}")
                 if r.status_code in BUSY_CODES:
                     # Rate limited - Switch to cooldown mode for 5 minutes
@@ -413,7 +413,7 @@ class TEDAPI:
             pb.tail.value = 1
             url = f'https://{self.gw_ip}/tedapi/v1'
             try:
-                r= self.session.post(url, verify=False, data=pb.SerializeToString(), timeout=self.timeout)
+                r= self.session.post(url, data=pb.SerializeToString(), timeout=self.timeout)
                 log.debug(f"Response Code: {r.status_code}")
                 if r.status_code in BUSY_CODES:
                     # Rate limited - Switch to cooldown mode for 5 minutes
@@ -479,7 +479,7 @@ class TEDAPI:
             pb.tail.value = 1
             url = f'https://{self.gw_ip}/tedapi/v1'
             try:
-                r = self.session.post(url, verify=False, data=pb.SerializeToString(), timeout=self.timeout)
+                r = self.session.post(url, data=pb.SerializeToString(), timeout=self.timeout)
                 log.debug(f"Response Code: {r.status_code}")
                 if r.status_code in BUSY_CODES:
                     # Rate limited - Switch to cooldown mode for 5 minutes
@@ -572,7 +572,7 @@ class TEDAPI:
             pb.tail.value = 1
             url = f'https://{self.gw_ip}/tedapi/v1'
             try:
-                r = self.session.post(url, verify=False, data=pb.SerializeToString(), timeout=self.timeout)
+                r = self.session.post(url, data=pb.SerializeToString(), timeout=self.timeout)
                 log.debug(f"Response Code: {r.status_code}")
                 if r.status_code in BUSY_CODES:
                     # Rate limited - Switch to cooldown mode for 5 minutes
@@ -678,7 +678,7 @@ class TEDAPI:
             pb.message.payload.send.b.value = "{\"pwsComponentsFilter\":{\"types\":[\"PW3SAF\"]},\"pwsSignalNames\":[\"PWS_SelfTest\",\"PWS_PeImpTestState\",\"PWS_PvIsoTestState\",\"PWS_RelaySelfTest_State\",\"PWS_MciTestState\",\"PWS_appGitHash\",\"PWS_ProdSwitch_State\"],\"pchComponentsFilter\":{\"types\":[\"PCH\"]},\"pchSignalNames\":[\"PCH_State\",\"PCH_PvState_A\",\"PCH_PvState_B\",\"PCH_PvState_C\",\"PCH_PvState_D\",\"PCH_PvState_E\",\"PCH_PvState_F\",\"PCH_AcFrequency\",\"PCH_AcVoltageAB\",\"PCH_AcVoltageAN\",\"PCH_AcVoltageBN\",\"PCH_packagePartNumber_1_7\",\"PCH_packagePartNumber_8_14\",\"PCH_packagePartNumber_15_20\",\"PCH_packageSerialNumber_1_7\",\"PCH_packageSerialNumber_8_14\",\"PCH_PvVoltageA\",\"PCH_PvVoltageB\",\"PCH_PvVoltageC\",\"PCH_PvVoltageD\",\"PCH_PvVoltageE\",\"PCH_PvVoltageF\",\"PCH_PvCurrentA\",\"PCH_PvCurrentB\",\"PCH_PvCurrentC\",\"PCH_PvCurrentD\",\"PCH_PvCurrentE\",\"PCH_PvCurrentF\",\"PCH_BatteryPower\",\"PCH_AcRealPowerAB\",\"PCH_SlowPvPowerSum\",\"PCH_AcMode\",\"PCH_AcFrequency\",\"PCH_DcdcState_A\",\"PCH_DcdcState_B\",\"PCH_appGitHash\"],\"bmsComponentsFilter\":{\"types\":[\"PW3BMS\"]},\"bmsSignalNames\":[\"BMS_nominalEnergyRemaining\",\"BMS_nominalFullPackEnergy\",\"BMS_appGitHash\"],\"hvpComponentsFilter\":{\"types\":[\"PW3HVP\"]},\"hvpSignalNames\":[\"HVP_State\",\"HVP_appGitHash\"],\"baggrComponentsFilter\":{\"types\":[\"BAGGR\"]},\"baggrSignalNames\":[\"BAGGR_State\",\"BAGGR_OperationRequest\",\"BAGGR_NumBatteriesConnected\",\"BAGGR_NumBatteriesPresent\",\"BAGGR_NumBatteriesExpected\",\"BAGGR_LOG_BattConnectionStatus0\",\"BAGGR_LOG_BattConnectionStatus1\",\"BAGGR_LOG_BattConnectionStatus2\",\"BAGGR_LOG_BattConnectionStatus3\"]}"
             pb.tail.value = 2
             url = f'https://{self.gw_ip}/tedapi/device/{pw_din}/v1'
-            r = self.session.post(url, verify=False, data=pb.SerializeToString(), timeout=self.timeout)
+            r = self.session.post(url, data=pb.SerializeToString(), timeout=self.timeout)
             if r.status_code == HTTPStatus.OK:
                 # Decode response
                 tedapi = tedapi_pb2.Message()
@@ -817,7 +817,7 @@ class TEDAPI:
             pb.tail.value = 2
             url = f'https://{self.gw_ip}/tedapi/device/{din}/v1'
             try:
-                r = self.session.post(url, verify=False, data=pb.SerializeToString(), timeout=self.timeout)
+                r = self.session.post(url, data=pb.SerializeToString(), timeout=self.timeout)
                 log.debug(f"Response Code: {r.status_code}")
                 if r.status_code in BUSY_CODES:
                     # Rate limited - Switch to cooldown mode for 5 minutes
@@ -855,7 +855,8 @@ class TEDAPI:
             status_forcelist=[429, 500, 502, 503, 504],
             raise_on_status=False
         )
-        adapter = HTTPAdapter(max_retries=retries, pool_connections=5, pool_maxsize=5)
+        adapter = HTTPAdapter(max_retries=retries, pool_connections=2, pool_maxsize=2)
+        session.verify = False
         session.mount("https://", adapter)
         session.auth = ('Tesla_Energy_Device', self.gw_pwd)
         session.headers.update({'Content-type': 'application/octet-string'})
@@ -889,7 +890,7 @@ class TEDAPI:
         self.din = None
         try:
             self.session = self._init_session()
-            resp = self.session.get(url, verify=False, timeout=5)
+            resp = self.session.get(url, timeout=5)
             if resp.status_code != HTTPStatus.OK:
                 # Connected but appears to be Powerwall 3
                 log.debug("Detected Powerwall 3 Gateway")
