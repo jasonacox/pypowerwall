5685dab17a94c2843831d98806e65cc97178ef60
Jason Cox <jason@jasonacox.com>
2026-03-25 21:51:08 -0700

    Add default empty list for battery_blocks in TEDAPI configuration

--- pypowerwall/tedapi/__init__.py
+++ pypowerwall/tedapi/__init__.py
@@ -347,12 +347,14 @@ class TEDAPI:
                     except json.JSONDecodeError as e:
                         log.error(f"Error Decoding JSON: {e}")
                         data = {}
+                    if 'battery_blocks' not in data:
+                        data["battery_blocks"] = []
                     log.debug(f"Configuration: {data}")
                     self.pwcachetime["config"] = time.time()
                     self.pwcache["config"] = data
                 except Exception as e:
                     log.error(f"Error fetching config: {e}")
-                data = None
+                    data = None
         return data
 
     def _write_config(self, updates: dict) -> bool:
